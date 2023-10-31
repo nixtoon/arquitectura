e2374a4772e741894abe22000d88edd8adf7f012
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-10-31 20:15:43 -0300

    reserva de hora listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/reserva-hora/reserva-hora.module.ts
+++ src/app/reserva-hora/reserva-hora.module.ts
@@ -8,12 +8,21 @@ import { ReservaHoraPageRoutingModule } from './reserva-hora-routing.module';
 
 import { ReservaHoraPage } from './reserva-hora.page';
 
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatNativeDateModule} from '@angular/material/core';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ReservaHoraPageRoutingModule
+    ReservaHoraPageRoutingModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule
   ],
   declarations: [ReservaHoraPage]
 })
